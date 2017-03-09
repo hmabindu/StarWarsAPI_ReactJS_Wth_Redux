7d6f437e14bb8a733aa52887403bd6b20a48c9c9
bindu bhupathi <bindubhupathi12@gmail.com>
2017-03-09 11:23:17 -0500

    Configured Redux Dev tools Extension in Store

--- src/app/store/store.js
+++ src/app/store/store.js
@@ -3,8 +3,10 @@ import thunk from 'redux-thunk';
 import reducers from '../reducers';
 
 
+
 const createStoreWithMiddleware = applyMiddleware(
     thunk
 )(createStore);
 
-export default createStoreWithMiddleware(reducers)
+
+export default createStoreWithMiddleware(reducers,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
